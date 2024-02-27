9d1e4dedc02b37cb948c8a0ce282421c26c97a44
oussemaha <oussemahamouda6@gmail.com>
2024-02-27 21:37:12 +0100

    version executed on WSL

--- RMI_client.java
+++ RMI_client.java
@@ -1,10 +1,17 @@
 import java.rmi.Naming;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+interface MyRemoteInterface extends Remote {
+    String sayHello() throws RemoteException;
+}
+
 
 public class RMI_client {
     public static void main(String[] args) {
         try {
             // Look up the remote object from the RMI registry
-            MyRemoteInterface remoteService = (MyRemoteInterface) Naming.lookup("rmi://localhost/RMI_s");
+            MyRemoteInterface remoteService = (MyRemoteInterface) Naming.lookup("rmi://172.31.96.1/RMI_s");
             // Call the remote method
             String message = remoteService.sayHello();
             System.out.println("Message from server: " + message);
